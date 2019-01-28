5f941234d8b586a0c3f8b58461aea7c88e3142f3
NathanT-W <nathtw1@gmail.com>
2019-01-28 19:54:32 +0000

    Dans Menu and Project Setup

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void BackToMainMenu() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
