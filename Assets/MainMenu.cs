f4bf848061b0124ff4f9ffc87c52498486de07dc
NathanT-W <nathtw1@gmail.com>
2019-02-01 12:33:39 +0000

    Remove unnescesary code

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -10,8 +10,4 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void GameQuit() {
-        Debug.Log("Quit");
-        Application.Quit();
-    }
 }
