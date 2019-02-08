a5c3923b003cbc40f09f77d62c13b54796cd5193
DanielJRaymond <daniel.raymond16@my.northampton.ac.uk>
2019-02-08 09:04:10 +0000

    Ball movement

---
+++ Assets/BallMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float Speed;
+
+    private Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float hMove = Input.GetAxis("Horizontal");
+        float vMove = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(hMove, vMove);
+
+        rb.AddForce(movement * Speed);
+
+    }
+}
