8f3532b33fd73a349da041152efc292ac3706414
Charlie Boulton <chas.boul97@gmail.com>
2019-04-26 13:29:17 +0100

    EndRestart

--- Assets/LevelComplete.cs
+++ Assets/LevelComplete.cs
@@ -3,8 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class LevelComplete : MonoBehaviour
 {
-    public void LoadNextLevel()
+    public void QuitGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Application.Quit();
     }
 }
